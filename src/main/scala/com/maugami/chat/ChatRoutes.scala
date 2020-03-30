69065a03ff241a24b47095d55af4461dcabaaa81
DeathSnork <0deathwing0@gmail.com>
2020-03-30 14:42:27 +0500

    Implement simple echo server for one user

---
+++ src/main/scala/com/maugami/chat/ChatRoutes.scala
@@ -0,0 +1,57 @@
+package com.maugami.chat
+
+import cats.effect.{Concurrent, Sync}
+import cats.implicits._
+import fs2.{Pipe, Stream}
+import fs2.concurrent.Queue
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.websocket.WebSocketBuilder
+import org.http4s.websocket.WebSocketFrame
+
+object ChatRoutes {
+
+  def jokeRoutes[F[_]: Sync](J: Jokes[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "joke" =>
+        for {
+          joke <- J.get
+          resp <- Ok(joke)
+        } yield resp
+    }
+  }
+
+  def helloWorldRoutes[F[_]: Sync](H: HelloWorld[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "hello" / name =>
+        for {
+          greeting <- H.hello(HelloWorld.Name(name))
+          resp <- Ok(greeting)
+        } yield resp
+    }
+  }
+
+  def chatRoutes[F[_]: Sync : Concurrent]: HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "ws" / name =>
+
+        val q = Queue.unbounded[F, WebSocketFrame]
+
+        q.flatMap(actualQ => {
+          def toClient: Stream[F, WebSocketFrame] = actualQ.dequeue
+
+          def fromClient: Pipe[F, WebSocketFrame, Unit] = actualQ.enqueue
+
+
+
+          WebSocketBuilder[F].build(toClient, fromClient)
+        })
+    }
+  }
+}
